a92ec03a6424fd723d3db2ee22c80edf47118e06
i-am-jiwon <dlark1128@gmail.com>
2023-11-30 17:07:01 +0900

    2-10

--- src/main/java/com/ll/sbbmission/question/question/controller/QuestionController.java
+++ src/main/java/com/ll/sbbmission/question/question/controller/QuestionController.java
@@ -7,17 +7,20 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/question")
 public class QuestionController {
 
     private final QuestionService questionService;
 
-    @GetMapping("/question/list")
+    @GetMapping("/list")
     public String list(Model model){
 
         List<Question> questionsList = this.questionService.getList();
@@ -25,4 +28,13 @@ public class QuestionController {
 
         return "question_list";
     }
+
+    @GetMapping(value = "detail/{id}")
+    public String detail(Model model, @PathVariable("id") Integer id){
+        Question question = this.questionService.getQuestion(id);
+        model.addAttribute("question", question);
+        return "question_detail";
+    }
+
 }
+
--- src/main/java/com/ll/sbbmission/question/question/service/QuestionService.java
+++ src/main/java/com/ll/sbbmission/question/question/service/QuestionService.java
@@ -1,11 +1,14 @@
 package com.ll.sbbmission.question.question.service;
 
+import com.ll.sbbmission.DataNotFoundException;
 import com.ll.sbbmission.question.question.entity.Question;
 import com.ll.sbbmission.question.question.repository.QuestionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -14,6 +17,16 @@ public class QuestionService {
 
     public List<Question> getList(){
         return this.questionRepository.findAll();
+    }
+
+    public Question getQuestion(Integer id) {
+        Optional<Question> question = this.questionRepository.findById(id);
+        if (question.isPresent()) {
+            return question.get();
+        }
+        else{
+            throw new DataNotFoundException("question not found");
+        }
     }
 
 }
